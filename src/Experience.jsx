b783f0513ecedd8c00cf294bf5a49bbf6e25e2cb
Tudish <lokatorist2@inbox.lv>
2024-07-26 21:26:22 +0300

    Created a level

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,15 @@
 import { OrbitControls } from '@react-three/drei'
 import Lights from './Lights.jsx'
-import Level from './Level.js'
-import { Physics} from '@react-three/rapier'
+import { BlockStart, Level } from './Level.js'
+import { Physics } from '@react-three/rapier'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
-
         <OrbitControls makeDefault />
         <Physics debug>
             <Lights />
             <Level/>
+            {/* <BlockStart/> */}
         </Physics>
-        
     </>
 }
